4f233b0f01553451a1d13f06e6f7f3b58ba27fa3
Nav31 <nsingh3104@gmail.com>
2016-05-05 18:03:04 -0400

    completed signup for users

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -4,6 +4,37 @@ app.config($stateProvider => {
 	$stateProvider.state('signup', {
 		url: '/signup',
 		templateUrl: 'js/signup/signup.html',
-		controller: 'LoginCtrl'
+		controller: 'SignupCtrl'
 	});
 })
+
+app.factory('UserFactory', ($http) => {
+	var UserFactory = {};
+	var parseData = res => res.data;
+
+	UserFactory.createUser = (body) => {
+		console.log(body)
+		return $http.post('/api/users/', body)
+		.then(parseData);
+	}
+
+	return UserFactory;
+})
+
+app.controller('SignupCtrl', ($scope, AuthService, $state, UserFactory) => {
+
+	$scope.signup = {};
+	$scope.error = null;
+
+	$scope.sendSignup = (signupInfo) => {
+		$scope.error = null; 
+
+		UserFactory.createUser(signupInfo)
+		.then(() => {
+			AuthService.login(signupInfo)
+			.then(() => $state.go('home'))
+			.catch(() => $scope.error = 'Invalid signup credentials.')
+		})
+	}
+})
+
