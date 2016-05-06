e3b84fef3508b4320fff5cdc45807617b69fc137
Anthony Velli <anthony.velli@gmail.com>
2016-05-06 05:28:30 -0400

    component route test completed.

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,10 +3,10 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
-router.use('/api/components', require('./components'));
-router.use('/api/screenplays', require('./screenplays'));
-router.use('/api/users', require('./user'));
-router.use('/api/character', require('./character'));
+router.use('/components', require('./components'));
+router.use('/screenplays', require('./screenplays'));
+router.use('/users', require('./user'));
+router.use('/character', require('./character'));
 
 // Make sure this is after all of
 // the registered routes!
--- server/db/models/scene.js
+++ server/db/models/scene.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var _ = require('lodash');
 
 var schema = new mongoose.Schema({
-    header: {type: String, required: true}
+    header: {type: String, required: true},
     components: [
         {type: mongoose.Schema.Types.ObjectId, ref: 'Component', index: true}
     ],
@@ -16,20 +16,21 @@ var schema = new mongoose.Schema({
     }
 });
 
-schema.pre('save', function(next) {
+schema.pre('validate', function(next) {
     var Component = mongoose.model('Component');
     
     if (this.components.length === 0) {next(); }
     var compstoPop = this.components.filter((ele) => !ele.type);
     var comps = this.components.filter((ele) => ele.type);
     if (comps.length > 0) {
-        this.header = this.header = comps.find(ele => ele.type === 'location');
+        this.header = comps.find(ele => ele.type === 'location').text;
+        next();
     }
     if (compstoPop.length === 0){next(); }
     Component.find({'_id': { $in: compstoPop}})
     .then(foundComps => {
         if (!foundComps) {next(); }
-        this.header = foundComps.find(ele => ele.type === 'location');
+        this.header = foundComps.find(ele => ele.type === 'location').text;
         next();
     });
 });
