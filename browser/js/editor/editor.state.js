75262dd6c7a54a68b7e8ac36bafce168c83643aa
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-05-07 13:34:39 -0400

    adds options to the editor

--- browser/js/editor/editor.state.js
+++ browser/js/editor/editor.state.js
@@ -16,4 +16,17 @@ app.config($stateProvider => {
         console.log($scope.tinymceModel);
     }
 
+    $scope.tinymceOptions = {
+          menu: {
+            file: {title: 'File', items: 'newdocument'},
+            edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+            insert: {title: 'Insert', items: 'link media | template hr'},
+            view: {title: 'View', items: 'visualaid'},
+            format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+            table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+            tools: {title: 'Tools', items: 'spellchecker code'}
+          },
+          min_height: 400
+    };
+
 })
