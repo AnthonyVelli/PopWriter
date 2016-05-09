70bca4fc6155f23ecf487af848de628c856d6999
Elizabeth Funk <elizabeth_funk@hotmail.com>
2016-05-09 14:12:00 -0400

    cleaned up scenes directive and controller

--- browser/js/scenes/scenes.js
+++ browser/js/scenes/scenes.js
@@ -6,22 +6,13 @@ app.directive('scenes', function ($state) {
             screenplay: '=' 
         },
         templateUrl: 'js/scenes/scenes.html',
-        controller: 'ScenesCtrl',
-        link: function (scope) {
-            console.log("screenplay from directive", scope.screenplay);
-            // code . . .
-        }
+        controller: 'ScenesCtrl'
     };
-
 });
 
-app.controller('ScenesCtrl', function($scope, $state, $stateParams){
-    $scope.screenplay = $stateParams.screenplay;
-    console.log("contrl screenplay", $scope.screenplay);
+app.controller('ScenesCtrl', function($scope){
     $scope.showform = false;
-    console.log("should be false", $scope.showform);
     $scope.showForm = function() {
         $scope.showform = true;
-        console.log("should be true", $scope.showform);
     };
 });
