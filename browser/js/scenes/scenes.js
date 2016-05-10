953a8d81be14f12aa07a4ead22446cc0e5b00a04
Elizabeth Funk <elizabeth_funk@hotmail.com>
2016-05-10 12:32:20 -0400

    fixes scene add functionality

--- browser/js/scenes/scenes.js
+++ browser/js/scenes/scenes.js
@@ -10,12 +10,25 @@ app.directive('scenes', function ($state) {
     };
 });
 
-app.controller('ScenesCtrl', function($scope){
+app.controller('ScenesCtrl', function($scope, SceneFactory, $state){
     $scope.showform = false;
     $scope.showForm = function() {
         $scope.showform = true;
     };
     $scope.hideForm = function() {
         $scope.showform = false;
+    };
+    $scope.addNewScene = function (screenplay, newscene){
+        console.log("newscene", newscene);
+        screenplay.scenes.push(newscene);
+        SceneFactory.addOne(screenplay)
+        .then(function(updatedScreenplay) {
+            console.log("updatedScreenplay", updatedScreenplay);
+            console.log($scope.screenplay);
+            // be sure to save data in editor before updating editor state
+            $state.go('editor', {id: screenplay._id});
+        });
+        $scope.showform = false;
+
     };
 });
