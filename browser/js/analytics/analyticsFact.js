7fc48f8fac0b905ca65cde18498066870bf1a4be
Anthony Velli <anthony.velli@gmail.com>
2016-05-14 16:55:53 -0400

    pull request to satisfy nav

--- browser/js/analytics/analyticsFact.js
+++ browser/js/analytics/analyticsFact.js
@@ -2,7 +2,7 @@ app.factory('AnalyticsFactory', ($http) => {
 	const parseData = res => res.data;
 	return {
 		getScreenPlays: () => $http.get('/api/analytics').then(parseData),
-		getSentiment: (id) => $http.get('/api/analytics/' + id).then(parseData),
+		getSentiment: (id) => $http.get('/api/analytics/' + id + '/emotion').then(parseData),
 		getCharacters: (id) => $http.get('/api/analytics/' + id + "/characters").then(parseData)
 	}
 });
