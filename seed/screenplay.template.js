2bef6a2f3e549b95916a129fb30f564495df7d03
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-05-16 12:25:32 -0400

    adds a screenplay seed file to the project folder

---
+++ seed/screenplay.template.js
@@ -0,0 +1,94 @@
+module.exports = {
+    screenplay: {
+        title: 'TITLE'
+    },
+    scenes: [{
+            components: [],
+            synopsis: 'something',
+            header: 'something'
+        },
+        {
+            components: [],
+            synopsis: 'something',
+            header: 'something'
+        },
+        {
+            components: [],
+            synopsis: 'something',
+            header: 'something'
+        },
+        {
+            components: [],
+            synopsis: 'something',
+            header: 'something'
+        },
+        {
+            components: [],
+            synopsis: 'something',
+            header: 'something'
+        },
+        {
+            components: [],
+            synopsis: 'something',
+            header: 'something'
+        }
+        ],
+    components: [[{
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            }],
+        [{
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            }],
+            [{
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            },
+            {
+            type: '',
+            text: ''
+            }]
+        ],
+
+}
+
+
+
+
+
+
+
+
