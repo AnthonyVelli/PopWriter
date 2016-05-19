e14044623582bb55f143e8d79c8480b58e077fd3
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-05-19 12:19:15 -0400

    refactor scene stte controller

--- browser/js/scenes/scenes.js
+++ browser/js/scenes/scenes.js
@@ -7,7 +7,7 @@ app.directive('scenes', function ($state) {
     };
 });
 
-app.controller('ScenesCtrl', function($scope, SceneFactory, $state, ScreenplaysFactory){
+app.controller('ScenesCtrl', function($scope, SceneFactory, $state, ScreenplaysFactory, EditorFactory){
     var draggingElements = document.getElementsByClassName('scene');
     $scope.showform = false;
     $scope.showForm = function() {
@@ -22,7 +22,7 @@ app.controller('ScenesCtrl', function($scope, SceneFactory, $state, ScreenplaysF
     $scope.onDropComplete = function (screenplay, newIdx, oldIdx){
         var valToMove = screenplay.scenes.splice(oldIdx, 1)[0];
         screenplay.scenes.splice(newIdx, 0, valToMove);
-        $scope.text = scriptify(screenplay);
+        $scope.text = EditorFactory.scriptify(screenplay);
         ScreenplaysFactory.updateScreenplay(screenplay._id, screenplay)
         .then(savedScreenplay => {
             console.log('screenplay saved!');
