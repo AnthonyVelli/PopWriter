d9d8bb0e82f6fedcc8ab183acfc2eba6010538e3
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-05-21 16:20:08 -0400

    removes console logs

--- browser/js/common/directives/scroll.directive.js
+++ browser/js/common/directives/scroll.directive.js
@@ -5,10 +5,8 @@ app.directive("scroll", function ($window) {
         angular.element($window).bind("scroll", function() {
              if (this.pageYOffset >= 100) {
                  element.addClass('fixed');
-                 console.log('Scrolled below header.');
              } else {
                  element.removeClass('fixed');
-                 console.log('Header is in view.');
              }
         });
     };
